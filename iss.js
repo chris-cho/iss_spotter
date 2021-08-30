660ed15089b1f745bb7003c1472a9d55a9453b01
Chris Cho <hyunsangc@gmail.com>
2021-08-29 21:35:58 -0400

    add coordinates to ip fetcher

--- iss.js
+++ iss.js
@@ -7,6 +7,7 @@
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 const request = require('request');
+const apiKey = "";
 
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
@@ -19,4 +20,16 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://geo.ipify.org/api/v1?apiKey=at_xjXqymNdY4rPAFiC6p8MeQ7M1w78V&ipAddress=${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200) {
+      return callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP. Response: ${body}`), null);
+    }
+    return callback(null, JSON.parse(body).location.lat, JSON.parse(body).location.lng);
+  });
+
+ 
+};
+
+module.exports = { fetchMyIP , fetchCoordsByIP };
